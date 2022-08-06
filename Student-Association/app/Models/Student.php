fe371972ea015b38e8cc7da6340ea74503d07698
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-06 11:20:03 +0600

    One to one and One to Many Added

---
+++ Student-Association/app/Models/Student.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Department;
+use App\Models\Course;
+
+class Student extends Model
+{
+    use HasFactory;
+    
+    public $timestamps = false;
+    protected $table = 'students';
+    protected $fillable = [
+        'name',
+        'course_id',
+        'department_id',
+        'image'
+    ];
+
+    public function department(){
+        return $this->belongsTo(Department::class);
+    }
+
+    public function course(){
+        return $this->hasMany(Course::class);
+    }
+}
